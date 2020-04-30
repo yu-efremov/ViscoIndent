e41c3619298ac4064a32719a8ad92631e595f6ad
yu-efremov <58818457+yu-efremov@users.noreply.github.com>
2020-04-30 15:41:50 +0300

    Update tingFC_constructor.py

--- tingFC_constructor.py
+++ tingFC_constructor.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-code for the construction of force-indentation curves
+Created on Wed Apr  8 21:52:06 2020
+
+@author: Yuri
 """
 import numpy as np
-import matplotlib.pyplot as plt
 from numpy import pi, tan, sin
+import matplotlib.pyplot as plt
 
 from Ting_numerical import ting_numerical
-from relaxation_functions import relaxation_function
 
 def tingFC_constructor(Pars, indentationfull):
     modelprobe = Pars['probe_shape']
@@ -31,18 +32,18 @@ def tingFC_constructor(Pars, indentationfull):
     Depth = indentationfull[MaxInd]
     Speed = Depth/time[MaxInd]
 
-    if indpars[0]==1: # [yes/no; depth; speed; numpoimts; ramp/sin];
-        Depth=indpars[1]  # [nm]
-        Speed=indpars[2]  # [nm/s]
-        MaxInd=indpars[3]  # num points in approach-indentation
-        dT2=Depth/Speed/MaxInd
+    if indpars[0] == 1: # [yes/no; depth; speed; numpoimts; ramp/sin];
+        Depth = indpars[1]  # [nm]
+        Speed = indpars[2]  # [nm/s]
+        MaxInd = int(indpars[3])  # num points in approach-indentation
+        dT2 = Depth/Speed/MaxInd
         dT = dT2  # suppress original dT
         PointN = MaxInd*2
         time = np.linspace(0,dT*(PointN-1), PointN)
         timeL = np.linspace(dT,dT*(PointN*2-1), PointN*2)
         ind_ramp = np.zeros([MaxInd*2])
         ind_rampL = np.zeros([MaxInd*4])
-        if indpars[4]==0: # ramp or sin
+        if indpars[4] == 0: # ramp or sin
             for ii in range(MaxInd):
                 ind_ramp[ii] = Speed*time[ii]
 
@@ -55,22 +56,20 @@ def tingFC_constructor(Pars, indentationfull):
             for ii in range(2*MaxInd, MaxInd*4):
                 ind_rampL[ii] = Speed*timeL[-1]-Speed*time[MaxInd]-Speed*timeL[ii]-Speed*dT;
                 
-        elif indpars[4]==1:
-            Freq=Speed/Depth/4
-            ind_ramp=Depth*sin(2*pi*Freq*time) # -1/4period
-            ind_rampL=Depth*sin(2*pi*Freq*timeL-pi/2)  # -1/2period
+        elif indpars[4] == 1:
+            Freq = Speed/Depth/4
+            ind_ramp = Depth*sin(2*pi*Freq*time) # -1/4period
+            ind_rampL = Depth*sin(2*pi*Freq*timeL-pi/2)  # -1/2period
         
-        indentationfull=ind_ramp
-        indentationfullL=ind_rampL
+        indentationfull = ind_ramp
+        indentationfullL = ind_rampL
         
-    [force, cradius, contact_time, t1_ndx2, Et] = ting_numerical(pars, Poisson, probe_dim, dT, MaxInd, Height, modelting, modelprobe, indentationfull)[0:5]
+    [force, cradius, contact_time, t1_ndx2, Et] = ting_numerical(pars,
+        Poisson, probe_dim, dT, MaxInd, Height, modelting, modelprobe, indentationfull)[0:5]
 
     forceL = np.pad(force, MaxInd, 'constant', constant_values=(0, 0))
     
     return time, indentationfull, force, cradius, indentationfullL, forceL
-    
-    
-    
     
     
 if __name__ == '__main__':
@@ -81,12 +80,16 @@ if __name__ == '__main__':
     Pars['dT'] = 1e-3                 # Sampling time (time step)
 
     Pars['height'] = 0
-    Pars['indpars'] = np.array([1, 50, 50, 1000, 1])
+    Pars['indpars'] = np.array([1, 50, 50, 500, 0]) # [yes/no; depth; speed; numpoimts; ramp/sin];
     
     Pars['viscomodel'] = 'sPLR'
-    Pars['Vpars'] = np.array([1000, 0.8, 0, 20])
-    indentationfull = np.array([0, 1])
+    Pars['Vpars'] = np.array([1000, 0.4, 0, 0])
+    
+    indentationfull = np.array([0, 1]) # arbitrary indentation profile
     time, indentationfull, force, cradius, indentationfullL, forceL = tingFC_constructor(Pars, indentationfull)
+    # curvedata = [time, indentationfull, force, cradius, indentationfullL, forceL]
+    # arr = np.vstack([curvedata[0], curvedata[1], curvedata[2]])
+    # arr.transpose()
     plt.figure(num='Force vs Indentation')
     plt.plot(indentationfull, force)
     plt.xlabel('Indentation')
